6b46fc410ecfe29d7b62a3ceedbf5f8c6a722e90
Frank He <heshanfu@gmail.com>
2019-02-13 21:12:50 +0800

    Update previews.js

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -7,6 +7,7 @@ const BlogPreview = ({ entry, widgetFor }) => {
         <div className="row">
           <div className="title">
             <span className="date">{date}</span>
+            <span className="tag">{tag}</span>
             <h1>{data.title}</h1>
           </div>
         </div>
